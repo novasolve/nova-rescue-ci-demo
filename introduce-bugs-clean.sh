0c2e102caafca27a33aa3cfeaf329939a3c7d0b1
Your Name <you@example.com>
2025-08-27 13:11:47 -0700

    Fix sed commands in introduce-bugs-clean.sh

--- introduce-bugs-clean.sh
+++ introduce-bugs-clean.sh
@@ -11,40 +11,40 @@ fi
 
 # Now introduce bugs WITHOUT any comments
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  # Fix add() method - change + to -
-  sed -i '' 's/def add(self, a: float, b: float) -> float:/&\n        """Add two numbers."""\n        return a - b/' src/calculator.py
-  sed -i '' '/def add/,/return a + b/{s/return a + b/return a - b/}' src/calculator.py
+  # macOS version
+  # Break add() - change + to -
+  sed -i '' 's/return a + b/return a - b/' src/calculator.py
   
   # Break subtract() - change - to +
-  sed -i '' '/def subtract/,/return a - b/{s/return a - b/return a + b/}' src/calculator.py
+  sed -i '' 's/return a - b/return a + b/' src/calculator.py
   
   # Break multiply() - change * to +
-  sed -i '' '/def multiply/,/return a \* b/{s/return a \* b/return a + b/}' src/calculator.py
+  sed -i '' 's/return a \* b/return a + b/' src/calculator.py
   
   # Break divide() - change / to *
-  sed -i '' '/def divide/,/return a \/ b/{s/return a \/ b/return a * b/}' src/calculator.py
+  sed -i '' 's/return a \/ b/return a * b/' src/calculator.py
   
   # Break power() - change ** to *
-  sed -i '' '/def power/,/return base \*\* exponent/{s/return base \*\* exponent/return base * exponent/}' src/calculator.py
+  sed -i '' 's/return base \*\* exponent/return base * exponent/' src/calculator.py
   
   # Break square_root() - return n instead of sqrt(n)
-  sed -i '' '/def square_root/,/return math\.sqrt(n)/{s/return math\.sqrt(n)/return n/}' src/calculator.py
+  sed -i '' 's/return math\.sqrt(n)/return n/' src/calculator.py
   
   # Break percentage() - multiply by 10 instead of dividing by 100
-  sed -i '' '/def percentage/,/return (value \* percent) \/ 100/{s/return (value \* percent) \/ 100/return (value * percent) * 10/}' src/calculator.py
+  sed -i '' 's/return (value \* percent) \/ 100/return (value * percent) * 10/' src/calculator.py
   
   # Break average() - return sum instead of average
-  sed -i '' '/def average/,/return sum(seq) \/ len(seq)/{s/return sum(seq) \/ len(seq)/return sum(seq)/}' src/calculator.py
+  sed -i '' 's/return sum(seq) \/ len(seq)/return sum(seq)/' src/calculator.py
 else
   # Linux version
-  sed -i '/def add/,/return a + b/{s/return a + b/return a - b/}' src/calculator.py
-  sed -i '/def subtract/,/return a - b/{s/return a - b/return a + b/}' src/calculator.py
-  sed -i '/def multiply/,/return a \* b/{s/return a \* b/return a + b/}' src/calculator.py
-  sed -i '/def divide/,/return a \/ b/{s/return a \/ b/return a * b/}' src/calculator.py
-  sed -i '/def power/,/return base \*\* exponent/{s/return base \*\* exponent/return base * exponent/}' src/calculator.py
-  sed -i '/def square_root/,/return math\.sqrt(n)/{s/return math\.sqrt(n)/return n/}' src/calculator.py
-  sed -i '/def percentage/,/return (value \* percent) \/ 100/{s/return (value \* percent) \/ 100/return (value * percent) * 10/}' src/calculator.py
-  sed -i '/def average/,/return sum(seq) \/ len(seq)/{s/return sum(seq) \/ len(seq)/return sum(seq)/}' src/calculator.py
+  sed -i 's/return a + b/return a - b/' src/calculator.py
+  sed -i 's/return a - b/return a + b/' src/calculator.py
+  sed -i 's/return a \* b/return a + b/' src/calculator.py
+  sed -i 's/return a \/ b/return a * b/' src/calculator.py
+  sed -i 's/return base \*\* exponent/return base * exponent/' src/calculator.py
+  sed -i 's/return math\.sqrt(n)/return n/' src/calculator.py
+  sed -i 's/return (value \* percent) \/ 100/return (value * percent) * 10/' src/calculator.py
+  sed -i 's/return sum(seq) \/ len(seq)/return sum(seq)/' src/calculator.py
 fi
 
 echo "✅ Bugs introduced (without any BUG comments)!"
